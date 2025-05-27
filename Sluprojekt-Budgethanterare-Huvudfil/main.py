d81f775ae3dc73258f3f7e1ab42d683f22d704a5
ValterCarlens <142878756+ValterCarlens@users.noreply.github.com>
2025-05-27 10:13:26 +0200

    Update main.py

--- Sluprojekt-Budgethanterare-Huvudfil/main.py
+++ Sluprojekt-Budgethanterare-Huvudfil/main.py
@@ -1,6 +1,14 @@
 import tkinter as tk
 from ui_main import BudgetManagerApp
 
+'''
+main.py: denna fil innehåller programmets huvudloop och huvudfönster
+
+__author__  = "Valter Carlens"
+__version__ = "1.0.0"
+__email__   = "valter.carlens@elev.ga.ntig.se"
+'''
+
 if __name__ == "__main__":
     root = tk.Tk()
     app = BudgetManagerApp(root)
